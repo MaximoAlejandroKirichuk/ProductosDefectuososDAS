5efd05f0a22b012d62e4da0e28a6b6a5ee0e5640
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2025-09-05 19:23:27 -0300

    cambios de string connection

--- DAL/StringConnection.cs
+++ DAL/StringConnection.cs
@@ -8,16 +8,30 @@ namespace DAL
 {
     public static class StringConnection
     {
-        private static string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-        public static string stringConnection =
-    "Server=tcp:das-proyecto.database.windows.net,1433;" +
-    "Initial Catalog=BaseDatos;" +
-    "Persist Security Info=False;" +
-    "User ID=maxdas;" +
-    $"Password={dbPassword};" +
-    "MultipleActiveResultSets=False;" +
-    "Encrypt=True;" +
-    "TrustServerCertificate=False;" +
-    "Connection Timeout=30;";
+        // Leer variables de entorno
+        private readonly static string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+        private readonly static string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        public static string stringConnection
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(dbUser) || string.IsNullOrEmpty(dbPassword))
+                    throw new InvalidOperationException(
+                        "Las variables de entorno DB_USER o DB_PASSWORD no están definidas."
+                    );
+
+                return
+                    $"Server=tcp:das-proyecto.database.windows.net,1433;" +
+                    "Initial Catalog=BaseDatos;" +
+                    "Persist Security Info=False;" +
+                    $"User ID={dbUser};" +      
+                    $"Password={dbPassword};" +
+                    "MultipleActiveResultSets=False;" +
+                    "Encrypt=True;" +
+                    "TrustServerCertificate=False;" +
+                    "Connection Timeout=30;";
+            }
+        }
     }
 }
